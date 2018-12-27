aa28440cb54c7457719d5fe2cd6279190402cafb
Tushar <tushar.gore@quagnitia.co.in>
2018-12-27 11:45:37 +0530

    Initial Commit

---
+++ MessagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by Tushar on 27/12/18.
+//  Copyright © 2018 Tushar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
